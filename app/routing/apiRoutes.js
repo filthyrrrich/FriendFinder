faa973290ddb8d2236541fccf66ff6df49abca5e
Rich Schiffer <richschiffer@Richs-MacBook-Pro.local>
2018-08-18 19:08:29 -0600

    updated comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,6 @@ module.exports = function(app) {
         
         //grabs the scores array from users input
         var scores = req.body.scores;
-        console.log(req.body);
-        //converts user input to integers
-        // var map1 = scores.map(x => parseInt(x));
         //loop thru friends
         var totalArray = [];
         for (let k = 0; k < friends.length; k++) {
@@ -21,35 +18,18 @@ module.exports = function(app) {
             //compare each friends scores to user input score and find difference
             var sum = scores.map(function (num, idx) {
                 return Math.abs(num - fArray[idx]);
-              });
+            });
             //finds total diff between user input and each friends
             var totalDiff = sum.reduce((a,b) => a + b, 0);
             //makes array of totals
             totalArray.push(totalDiff);
-
-            //console.log("totalDiffs:",totalArray);
         }
+
         //finds smallest number
         var bestMatch = Math.min.apply(Math,totalArray);
         //checks totalarray for position of smallest number
         var matched = totalArray.findIndex(match => match === bestMatch);
-
-        console.log(totalArray);
-        console.log(matched);
-
-        //grab friends[matched].name
-        console.log(friends[matched].name);
-        
-        //grab friends[matched].photo
-        console.log(friends[matched].photo);
-        console.log(req.body);
         res.json(friends[matched]);
-
-        
-        // console.log("Best MAtch",Math.min.apply(Math,totalArray));
-        // console.log(Math.min(totalArray));
-
-        //adds user input to friends API List
         friends.push(req.body);
     });
 }
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+//Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// Tells node that we are creating an "express" server
 var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Maps how server responds from various URLs
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
- 
+
+//Starts server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
